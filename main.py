00de0cdd875bf7400182e4cbdd75cd7a2c1afc38
s131800 <phippsee3@gmail.com>
2024-05-21 19:07:14 -0400

    Background Color and Button
    Testing background color and button

--- main.py
+++ main.py
@@ -1,9 +1,25 @@
 print("Hello World")
 from kivymd.app import MDApp
-from kivymd.uix.label import MDLabel
+from kivymd.uix.screen import MDScreen
+from kivymd.uix.button import MDButton, MDButtonText
+
+
 
 class MainApp(MDApp):
     def build(self):
-        return MDLabel(text="Hello World!", halign="center")
+        self.theme_cls.theme_style = "Light"
+        self.theme_cls.primary_palette = "Red"
+
+        return(
+            MDScreen(
+                MDButton(
+                    MDButtonText(
+                        text="Hello, World",
+                    ),
+                    pos_hint={"center_x": 0.5, "center_y": 0.5},
+                ),
+                md_bg_color=self.theme_cls.backgroundColor,
+            )
+        )
     
 MainApp().run()
