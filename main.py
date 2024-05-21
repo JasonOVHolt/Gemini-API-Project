fd825c31b83651d0ed44536fc2958d316dd5768e
Vindico <107150204+Vinadium@users.noreply.github.com>
2024-05-21 18:31:24 -0400

    Tutorial
    Basic Hello World to test GUI

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
 print("Hello World")
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel
+
+class MainApp(MDApp):
+    def build(self):
+        return MDLabel(text="Hello World!", halign="center")
+    
+MainApp().run()
